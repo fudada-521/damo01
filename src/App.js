5bbd0cbe1d39a41ad090e3052352b5b31730d7aa
fukun <fukun125@163.com>
2023-11-20 14:20:16 +0800

    123

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import { useState } from 'react';
+import './App.css';
+// import Game from './Game';
+import Login from './Login';
+
+
+export default function App() {
+
+
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
