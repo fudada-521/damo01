9fc57d17be238027ef221845a0d12f24b7574193
fukun <fukun125@163.com>
2023-11-21 17:25:57 +0800

    dotCurrentSize

--- src/Stepsbar/Timeline.js
+++ src/Stepsbar/Timeline.js
@@ -1,64 +1,7 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import { Steps, Switch } from 'antd';
+import { Steps, Switch, ConfigProvider } from 'antd';
 import { CaretRightOutlined, StepForwardOutlined } from "@ant-design/icons";
-// 年、月 模式切换
-function SwitchYearAndMonth({ playMode, setPlayMode }) {
-    return (
-        <Switch
-            size="small"
-            checkedChildren="年"
-            unCheckedChildren="月"
-            style={{
-                backgroundColor: 'rgba(9, 71, 149, 1)',
-            }}
-            checked={playMode}
-            onChange={setPlayMode}
-
-        />
-    );
-}
-// 播放、暂停按钮
-function ControllerWidget({ playingStatus, setPlayingStatus }) {
-    // 文字样式
-    var style = {
-        color: '#666666',
-        fontSize: '13px',
-        padding: '5px 0px',
-        width: '60px'
-    }
-
-    return (
-        <div style={style} onClick={setPlayingStatus}>
-            {!playingStatus ? <CaretRightOutlined /> : <StepForwardOutlined />}
-            <span style={style}>{!playingStatus ? '播放' : '暂停'}</span>
-        </div>
-    );
-}
-
-
-/// 步骤条Widget
-function StepsWidget({ items, current }) {
-    return (
-        <div style={{
-            display: 'inline-flex',
-            background: 'rgba(255, 255, 255, 0.3)',
-            borderRadius: '3px',
-            paddingLeft: '5px',
-            paddingRight: '5px',
-            paddingTop: '5px',
-            paddingBottom: '5px',
-        }}>
-            <Steps
-                progressDot
-                current={current}
-                direction="vertical"
-                items={items}
-            />
-
-        </div>
-    );
-}
 const itemsByYear = [
     {
         title: '2018'
@@ -74,6 +17,7 @@ const itemsByYear = [
     },
     {
         title: '2023'
+
     },
 
 ];
@@ -117,6 +61,78 @@ const itemsByMonth = [
 
 ];
 
+// 年、月 模式切换
+function SwitchYearAndMonth({ playMode, setPlayMode }) {
+    return (
+        <Switch
+            size="small"
+            checkedChildren="年"
+            unCheckedChildren="月"
+            style={{
+                backgroundColor: 'rgba(9, 71, 149, 1)',
+            }}
+            checked={playMode}
+            onChange={setPlayMode}
+        />
+    );
+}
+
+
+// 播放、暂停按钮
+function ControllerWidget({ playingStatus, setPlayingStatus }) {
+    // 文字样式
+    var style = {
+        color: '#666666',
+        fontSize: '13px',
+        padding: '5px 0px',
+        width: '60px'
+    }
+
+    return (
+        <div style={style} onClick={setPlayingStatus}>
+            {!playingStatus ? <CaretRightOutlined /> : <StepForwardOutlined />}
+            <span style={style}>{!playingStatus ? '播放' : '暂停'}</span>
+        </div>
+    );
+}
+
+/// 步骤条Widget
+function StepsWidget({ items, current }) {
+    return (
+        <div style={{
+            display: 'inline-flex',
+            background: 'rgba(255, 255, 255, 0.3)',
+            borderRadius: '3px',
+            paddingLeft: '5px',
+            paddingRight: '5px',
+            paddingTop: '5px',
+            paddingBottom: '5px',
+        }}>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Steps: {
+                            dotCurrentSize: '8px',
+                            fontSize: '11px',
+                        },
+                    },
+                }}
+            >
+                <Steps
+                    progressDot
+                    current={current}
+                    size="small"
+                    direction="vertical"
+                    items={items}
+                />
+            </ConfigProvider>
+
+
+        </div>
+    );
+}
+
+
 /// 时间条
 const TimelineComponent = ({ model, itmes, onChange }) => {
     // 播放模式：逐年/逐月
@@ -128,7 +144,6 @@ const TimelineComponent = ({ model, itmes, onChange }) => {
 
     // 切换播放模式
     function handlePlayModeChange() {
-        // 切换计时器状态
 
         // 如果计时器关闭，重置计数
         if (!isPlaying) {
