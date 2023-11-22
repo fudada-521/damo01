e470df48d4a2616fc1e05fb77ee0a39519f90439
fukun <fukun125@163.com>
2023-11-22 16:54:08 +0800

    123

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,18 +1,43 @@
 import React from "react";
-import "./Login.css"
+import "./Login.css";
 import TimelineComponent from "../Stepsbar/Timeline";
-
-
-
-
 
 // 页面主体
 export default function Login() {
-    return (
-        <>
-            <div className="login">
-                <TimelineComponent />
-            </div>
-        </>
-    );
+  const itmesForYear = ["2017", "2018", "2019", "2020", "2021", "2022", "2023"];
+  const itmesForMonth = [
+    "2017-01",
+    "2017-02",
+    "2017-03",
+    "2017-04",
+    "2017-05",
+    "2017-06",
+    "2017-07",
+    "2017-08",
+    "2017-09",
+    "2017-10",
+    "2017-11",
+    "2017-12",
+  ];
+
+  return (
+    <>
+      <div className="login">
+        <TimelineComponent
+          model={"year"}
+          itmes={itmesForYear}
+          onChange={() => {
+            console.log("触发回调");
+          }}
+        />
+        <TimelineComponent
+          model={"month"}
+          itmes={itmesForMonth}
+          onChange={() => {
+            console.log("触发回调");
+          }}
+        />
+      </div>
+    </>
+  );
 }
--- src/Stepsbar/Timeline.js
+++ src/Stepsbar/Timeline.js
@@ -1,202 +1,141 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import { Steps, Switch, ConfigProvider } from 'antd';
+import { useState, useEffect } from "react";
+import { Switch } from "antd";
 import { CaretRightOutlined, StepForwardOutlined } from "@ant-design/icons";
-const itemsByYear = [
-    {
-        title: '2018'
-    },
-    {
-        title: '2019'
-    },
-    {
-        title: '2020'
-    },
-    {
-        title: '2022'
-    },
-    {
-        title: '2023'
-
-    },
-
-];
-const itemsByMonth = [
-    {
-        title: '2019-01'
-    },
-    {
-        title: '2019-02'
-    },
-    {
-        title: '2019-03'
-    },
-    {
-        title: '2019-04'
-    },
-    {
-        title: '2019-05'
-    },
-    {
-        title: '2019-06'
-    },
-    {
-        title: '2019-07'
-    },
-    {
-        title: '2019-08'
-    },
-    {
-        title: '2019-09'
-    },
-    {
-        title: '2019-10'
-    },
-    {
-        title: '2019-11'
-    },
-    {
-        title: '2019-12'
-    },
-
-];
+import "./Timeline.css";
 
 // 年、月 模式切换
 function SwitchYearAndMonth({ playMode, setPlayMode }) {
-    return (
-        <Switch
-            size="small"
-            checkedChildren="年"
-            unCheckedChildren="月"
-            style={{
-                backgroundColor: 'rgba(9, 71, 149, 1)',
-            }}
-            checked={playMode}
-            onChange={setPlayMode}
-        />
-    );
+  return (
+    <Switch
+      size="small"
+      checkedChildren="年"
+      unCheckedChildren="月"
+      style={{
+        backgroundColor: "rgba(9, 71, 149, 1)",
+      }}
+      checked={playMode}
+      onChange={setPlayMode}
+    />
+  );
 }
-
 
 // 播放、暂停按钮
 function ControllerWidget({ playingStatus, setPlayingStatus }) {
-    // 文字样式
-    var style = {
-        color: '#666666',
-        fontSize: '13px',
-        padding: '5px 0px',
-        width: '60px'
-    }
+  // 文字样式
+  var style = {
+    color: "#666666",
+    fontSize: "13px",
+    padding: "5px 0px",
+    width: "60px",
+  };
 
-    return (
-        <div style={style} onClick={setPlayingStatus}>
-            {!playingStatus ? <CaretRightOutlined /> : <StepForwardOutlined />}
-            <span style={style}>{!playingStatus ? '播放' : '暂停'}</span>
-        </div>
-    );
+  return (
+    <div style={style} onClick={setPlayingStatus}>
+      {!playingStatus ? <CaretRightOutlined /> : <StepForwardOutlined />}
+      <span style={style}>{!playingStatus ? "播放" : "暂停"}</span>
+    </div>
+  );
 }
 
 /// 步骤条Widget
-function StepsWidget({ items, current }) {
-    return (
-        <div style={{
-            display: 'inline-flex',
-            background: 'rgba(255, 255, 255, 0.3)',
-            borderRadius: '3px',
-            paddingLeft: '5px',
-            paddingRight: '5px',
-            paddingTop: '5px',
-            paddingBottom: '5px',
-        }}>
-            <ConfigProvider
-                theme={{
-                    components: {
-                        Steps: {
-                            dotCurrentSize: '8px',
-                            fontSize: '11px',
-                        },
-                    },
-                }}
-            >
-                <Steps
-                    progressDot
-                    current={current}
-                    size="small"
-                    direction="vertical"
-                    items={items}
-                />
-            </ConfigProvider>
-
-
+function StepsWidget({ height, items, percent }) {
+  const itemHeight = height / items.length;
+  const itemHeight2 = height / (items.length - 1);
+  return (
+    <div className="bar" style={{ height: `${height + itemHeight}px` }}>
+      <div className="bar-body" style={{ height: `${height}px` }}>
+        {/* 进度条 */}
+        <div className="bar-outline">
+          <div className="bar-color" style={{ height: `${percent}%` }} />
         </div>
-    );
-}
 
+        {/* item */}
+        <div
+          className="bar-item-board"
+          style={{ height: `${height + itemHeight}px` }}
+        >
+          {items.map((item, index) => (
+            <div
+              className="bar-item"
+              key={index}
+              style={{
+                height: `${itemHeight2}px`,
+              }}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 /// 时间条
+/// model：播放模式，year，month
+/// items：时间轴节点
+/// onChange：播放过程中的回调
 const TimelineComponent = ({ model, itmes, onChange }) => {
-    // 播放模式：逐年/逐月
-    const [isByYear, setPlayMode] = useState(true);
-    // 播放控制：播放、暂停,默认未播放
-    const [isPlaying, setPlayingStatus] = useState(false);
-    const [timelineItems, setTimelineItems] = useState(itemsByYear);
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const height = model === "year" ? 200 : 300;
+  const length = itmes.length; // 一共需要几个时间轴节点
+  const size = 100.0 / (length - 1); // 每两个时间轴节点的之间（一段）占整个时间轴的百分比
 
-    // 切换播放模式
-    function handlePlayModeChange() {
+  // 播放模式：逐年/逐月
+  // const [playMode, setPlayMode] = useState(model === 'year');
 
-        // 如果计时器关闭，重置计数
-        if (!isPlaying) {
-            setCurrentIndex(0);
-        }
-        if (isByYear) {
-            setPlayMode(!isByYear);
-            setCurrentIndex(0);
-            setTimelineItems(itemsByMonth);
-            setPlayingStatus(false);
-        } else {
-            setPlayMode(!isByYear);
-            setCurrentIndex(0);
-            setTimelineItems(itemsByYear);
-            setPlayingStatus(false);
-        }
+  // 播放控制：播放、暂停,默认未播放
+  const [playingStatus, setPlayingStatus] = useState(false);
+
+  // 当前的进度百分比
+  const [percent, setPercent] = useState(0.0);
+
+  // 切换播放模式
+  // function handlePlayModeChange() {
+  //     if (!playingStatus) {
+  //         setPercent(0);
+  //     }
+  //     if (playMode) {
+  //         setPlayMode(!playMode);
+  //         setPlayingStatus(false);
+  //         setPercent(0);
+  //     } else {
+  //         setPlayMode(!playMode);
+  //         setPlayingStatus(false);
+  //         setPercent(0);
+  //     }
+  // }
+
+  // 播放状态
+  function handlePlayingStatus() {
+    setPlayingStatus(!playingStatus);
+  }
+
+  useEffect(() => {
+    let intervalId;
+    if (playingStatus) {
+      // 开始计时任务
+      intervalId = setInterval(() => {
+        setPercent((prevCount) => (prevCount >= 99 ? 0 : prevCount + size));
+      }, 1000);
+    } else {
+      // 清除定时器
+      clearInterval(intervalId);
     }
 
-    // 播放状态
-    function handlePlayingStatus() {
-        setPlayingStatus(!isPlaying);
-    }
-
-
-    useEffect(() => {
-        let intervalId;
-
-        if (isPlaying) {
-            // 开始计时任务
-            intervalId = setInterval(() => {
-                if (isByYear) {
-                    setCurrentIndex(prevCount => (prevCount >= 4 ? 0 : prevCount + 1));
-                } else {
-                    setCurrentIndex(prevCount => (prevCount >= 11 ? 0 : prevCount + 1));
-                }
-            }, 500);
-        } else {
-            // 清除定时器
-            clearInterval(intervalId);
-        }
-
-        // 在组件卸载时清除定时器
-        return () => clearInterval(intervalId);
-    }, [isPlaying, isByYear]);
-
-
-    return (
-        <>
-            <SwitchYearAndMonth playMode={isByYear} setPlayMode={() => handlePlayModeChange()} />
-            <ControllerWidget playingStatus={isPlaying} setPlayingStatus={() => handlePlayingStatus()} />
-            <StepsWidget items={timelineItems} current={currentIndex} />
-        </>
-    );
-}
+    // 在组件卸载时清除定时器
+    return () => clearInterval(intervalId);
+  }, [playingStatus, size]);
+  return (
+    <div style={{ display: "inline-block" }}>
+      {/* <SwitchYearAndMonth playMode={playMode} setPlayMode={() => handlePlayModeChange()} /> */}
+      <ControllerWidget
+        playingStatus={playingStatus}
+        setPlayingStatus={() => handlePlayingStatus()}
+      />
+      <StepsWidget height={height} items={itmes} percent={percent} />
+    </div>
+  );
+};
 
 export default TimelineComponent;
